0ba7dc488851492f75517dfdb492d95cd4feec11
Hélder Teixeira <corvisierpt@gmail.com>
2023-12-30 14:56:13 +0000

    Renamed folder name to match Filaments i18n structure (#59)
    * European Portuguese
    
    Added European Portuguese translation
    
    * Renamed folder name to match Filaments i18n structure
    
    Renamed folder name to match Filaments i18n structure

---
+++ resources/lang/pt_PT/backup.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+    'components' => [
+        'backup_destination_list' => [
+            'table' => [
+                'actions' => [
+                    'download' => 'Descarregar',
+                    'delete' => 'Eliminar',
+                ],
+
+                'fields' => [
+                    'path' => 'Caminho',
+                    'disk' => 'Disco',
+                    'date' => 'Data',
+                    'size' => 'Tamanho',
+                ],
+
+                'filters' => [
+                    'disk' => 'Disco',
+                ],
+            ],
+        ],
+
+        'backup_destination_status_list' => [
+            'table' => [
+                'fields' => [
+                    'name' => 'Nome',
+                    'disk' => 'Disco',
+                    'healthy' => 'Estado',
+                    'amount' => 'Quantidade',
+                    'newest' => 'Recente',
+                    'used_storage' => 'Espaço Utilizado',
+                ],
+            ],
+        ],
+    ],
+
+    'pages' => [
+        'backups' => [
+            'actions' => [
+                'create_backup' => 'Criar Cópia de Segurança',
+            ],
+
+            'heading' => 'Cópias de Segurança',
+
+            'messages' => [
+                'backup_success' => 'A criar uma nova cópia de segurança em segundo plano.',
+                'backup_delete_success' => 'A eliminar esta cópia de segurança em segundo plano.',
+            ],
+
+            'modal' => [
+                'buttons' => [
+                    'only_db' => 'Apenas BD',
+                    'only_files' => 'Apenas Ficheiros',
+                    'db_and_files' => 'BD & Ficheiros',
+                ],
+
+                'label' => 'Por favor, seleccione uma opção',
+            ],
+
+            'navigation' => [
+                'group' => 'Definições',
+                'label' => 'Cópias de Segurança',
+            ],
+        ],
+    ],
+
+];
