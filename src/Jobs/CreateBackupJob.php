04d68389260f26a849f8121f982ad5fbd25e9147
RChutchev <11131666+RChutchev@users.noreply.github.com>
2025-03-26 01:41:12 +0000

    Fix styling

--- src/Jobs/CreateBackupJob.php
+++ src/Jobs/CreateBackupJob.php
@@ -19,7 +19,8 @@ class CreateBackupJob implements ShouldQueue
     public function __construct(
         protected readonly Option $option = Option::ALL,
         protected readonly ?int $timeout = null,
-    ) {}
+    ) {
+    }
 
     public function handle(): void
     {
